877ed9c7a185b231009913872cc78c28d95fc438
mohammad <mfelfelani72@gmail.com>
2025-02-10 08:23:55 +0000

    editt

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -18,15 +18,25 @@ class AuthController extends Controller
         // Validate the incoming request
         $validator = Validator::make($request->all(), [
             'name' => 'required',
-            'email' => 'required|email',
-            'password' => 'required',
+            'email' => 'required|email|unique:users',
+            // 'password' => 'required|min:5|regex:/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).+$/',
+            'password' => 'required|min:5',
             'password_confirmation' => 'required|same:password',
+        ], [
+            'name.required' => 'name_required',
+            'email.required' => 'email_required',
+            'email.email' => 'email_not_available_email',
+            'email.unique' => 'email_not_unique',
+            'password.required' => 'password_required',
+            'password.min' => 'password_min_8',
+            // 'password.regex' => 'password_regex',
+            'password_confirmation.required' => 'password_confirmation_required',
+            'password_confirmation.same' => 'password_confirmation_same',
         ]);
 
-        if ($validator->fails()) {
-
+        if ($validator->fails())
             return response()->json(CreateResponseMessage::Error('error_in_validate', $validator->errors()), 500);
-        } else {
+        else {
 
             try {
                 // Create the user
@@ -40,10 +50,10 @@ class AuthController extends Controller
                 Auth::login($user);
 
                 // Return a success response
-                return response()->json(['message' => 'Registration successful', 'user' => $user], 201);
-            } catch (\Exception $e) {
+                return response()->json(CreateResponseMessage::Success('registration_successful', $user), 201);
+            } catch (\Exception $error) {
 
-                return response()->json(['message' => 'Error creating user', 'error' => $e->getMessage()], 500);
+                return response()->json(CreateResponseMessage::Error('error_in_create_user', $error), 500);
             }
         }
     }
